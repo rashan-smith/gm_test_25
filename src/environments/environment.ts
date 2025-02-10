15c157a43ff121c7aa89b8d81546a7f940b307c2
Victor Lopez <viclopez@unicef.com>
2025-02-10 14:11:49 +0100

    Update package and environment versions to 1.0.11

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   // restAPI: 'https://uni-connect-services.azurewebsites.net/api/v1/',
   restAPI: env.mode === 'dev' ? env.restAPIDev : env.restAPI,
   token: env.mode === 'dev' ? env.tokenDev : token,
-  app_version: '1.0.10',
+  app_version: '1.0.11',
   appName: 'Project Connect',
   appNameSuffix: 'Daily Check',
   showAboutMenu: true,
