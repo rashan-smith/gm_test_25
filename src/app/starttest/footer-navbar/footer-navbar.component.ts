50788b9a70781213f5f9ac0899295ea6095645c4
himsehra <himanshi.sehrawat@nagarro.com>
2025-04-09 17:44:27 +0530

    fixed

--- src/app/starttest/footer-navbar/footer-navbar.component.ts
+++ src/app/starttest/footer-navbar/footer-navbar.component.ts
@@ -1,26 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-footer-navbar',
   templateUrl: './footer-navbar.component.html',
   styleUrls: ['./footer-navbar.component.scss'],
 })
-export class FooterNavbarComponent implements OnInit {
+export class FooterNavbarComponent implements OnInit, OnDestroy{
   activeSegment: string = 'home';
+  private routerSubscription!: Subscription;
 
   constructor(private router: Router) { }
 
   ngOnInit() {
      // Subscribe to router events
-     this.router.events.subscribe(event => {
+     this.routerSubscription = this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
-        console.log(this.router.url)
+        console.log('hrihih',this.router.url)
         // Check the current URL and set the segment accordingly
-        if (this.router.url === '/starttest') {
+        if (this.router.url.startsWith('/starttest')) {
+          if(this.router.url.includes('/detail-page/')) {
+            this.activeSegment = "about"
+          } else {
           this.activeSegment = 'home';
-        } else if (this.router.url.startsWith('/starttest/detail-page/')) {
-          this.activeSegment = 'about';
+          }
         } else {
           // Default fallback if needed
           this.activeSegment = 'home';
@@ -34,8 +38,13 @@ export class FooterNavbarComponent implements OnInit {
     // If user taps segment, navigate accordingly
     if (event.detail.value === 'home') {
       this.router.navigate(['/starttest']);
+      this.activeSegment = "home"
     } else if (event.detail.value === 'about') {
       this.router.navigate(['/starttest/detail-page/23']);
+      this.activeSegment = "about"
     }
   }
+  ngOnDestroy() {
+   this.routerSubscription.unsubscribe();
+  } 
 }
